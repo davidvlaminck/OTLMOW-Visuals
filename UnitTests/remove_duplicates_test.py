876836d745d3727e0540637289cbdb82c1a42828
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-06-07 23:37:03 +0200

    added test

---
+++ UnitTests/remove_duplicates_test.py
@@ -0,0 +1,13 @@
+from otlmow_model.Classes.Onderdeel.Aftakking import Aftakking
+
+from otlmow_visuals.PyVisWrapper import remove_duplicates_in_iterable_based_on_asset_id
+
+
+def test_remove_duplicates_in_iterable_based_on_asset_id():
+    a = Aftakking()
+    a.assetId.identificator = '1'
+    b = Aftakking()
+    b.assetId.identificator = '1'
+    assets = [a, b]
+    assets = remove_duplicates_in_iterable_based_on_asset_id(assets)
+    assert len(assets) == 1
